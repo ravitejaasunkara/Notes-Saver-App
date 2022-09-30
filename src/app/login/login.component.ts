49baf71271fc040184d4335a9139d0de1e20b400
ravi <mooninthedarknight1999@gmail.com>
2022-09-30 11:47:56 +0530

    validations-css-updated
    - added some css
    - removed unnecessary validations for login and signup forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,12 +25,11 @@ export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
     username: new FormControl('',
       [
-        Validators.required, Validators.minLength(8)]),
+        Validators.required, Validators.minLength(6)]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('',
       [
-        Validators.required, Validators.minLength(8),
-        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+        Validators.required
       ]),
   });
 
